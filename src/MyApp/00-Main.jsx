d7cb314e823cb85800f917c00448daa52372a337
Edxael <zerphyr.r@gmail.com>
2018-07-09 12:34:35 -0600

    1st Commit

---
+++ src/MyApp/00-Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class extends React.Component{
+    render(){
+        return(
+            <div>
+                <h3>Hello from Main.</h3>
+            </div>
+        )
+    }
+}
