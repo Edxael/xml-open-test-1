ae50dbba993b22c39cd86a11b0027316984a4358
Edxael <zerphyr.r@gmail.com>
2018-07-09 13:14:43 -0600

    update on code

--- src/MyApp/00-Main.jsx
+++ src/MyApp/00-Main.jsx
@@ -4,7 +4,7 @@ export default class extends React.Component{
     render(){
         return(
             <div>
-                <h3>Hello from Main.</h3>
+                <h3>Hello from Main. 123</h3>
             </div>
         )
     }
